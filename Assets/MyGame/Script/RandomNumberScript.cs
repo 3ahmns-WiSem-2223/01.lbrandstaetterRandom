a271cb7ea79a0470f0ab0a211ae00bed53a1a6be
lbrandstaetter <luca.brandstaetter06@htl-salzburg.ac.at>
2022-11-20 15:02:45 +0100

    changes commited luca

--- Assets/MyGame/Script/RandomNumberScript.cs
+++ Assets/MyGame/Script/RandomNumberScript.cs
@@ -8,13 +8,13 @@ public class RandomNumberScript : MonoBehaviour
     private void Start()
     {
         float randomValue = UnityEngine.Random.value;
-        Debug.Log(randomValue);
+        Debug.Log(randomValue+ ("Random Value"));
 
 
         transform.position = UnityEngine.Random.insideUnitSphere * 5;
         Debug.Log(transform.position + "Transform Position");
 
-        GetComponent<Rigidbody>().velocity = Random.onUnitSphere * 10;
-        Debug.Log()
+        transform.rotation = UnityEngine.Random.rotation;
+        Debug.Log(transform.rotation + "Rotation");
     }
 }
